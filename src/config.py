bab3a216325767440ce376ee219079357d9e303a
Андрей Щеглов <Andrey.Scheglov@bia-tech.ru>
2025-03-17 18:32:05 +0300

    linter

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 import os
 
+
 class Settings:
     APP_PORT: int = int(os.getenv("APP_PORT", 8000))
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
     # Добавьте другие переменные конфигурации по необходимости
+
 
 settings = Settings()
