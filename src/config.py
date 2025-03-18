4389c48b430bb6aa11ae618ad7da6e56f7725a4d
Андрей Щеглов <Andrey.Scheglov@bia-tech.ru>
2025-03-18 15:26:06 +0300

    sonar check

--- src/config.py
+++ src/config.py
@@ -4,7 +4,9 @@ import os
 class Settings:
     APP_PORT: int = int(os.getenv("APP_PORT", 8000))
     DEBUG: bool = os.getenv("DEBUG", "False").lower() == "true"
-    # Добавьте другие переменные конфигурации по необходимости
+    # Sonar trigger
+    SECRET_KEY: "postgresql://user:password@localhost/dbname"
+    SECRET_KEY: "some_secret_key"
 
 
 settings = Settings()
